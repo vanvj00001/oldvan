3971e2d6fa554aae444d63dd14154b839df19b3b
范维骏 <fanweijun@fanweijundeMacBook-Air.local>
2026-03-09 21:44:24 +0800

    auto update

--- deploy.sh
+++ deploy.sh
@@ -8,9 +8,8 @@ echo "提交代码..."
 git add . 
 git commit -m "更新: $(date '+%Y-%m-%d %H:%M:%S')"
 
-echo "推送到 GitHub & Gitee..."
+echo "推送到 GitHub..."
 git push origin main
-git push gitee main
 
 echo "同步到服务器..."
 rsync -avz -e "ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no" --delete /Users/fanweijun/oldvan/public/ root@122.51.71.6:/www/wwwroot/oldvan/
